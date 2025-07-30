e3f91de203e2addec9415396dff0916975f52164
JALAL <eabduljalil@outlook.com>
2025-07-30 17:44:58 +0100

    Add runtime metrics endpoint and middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -13,6 +14,7 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
 	router.HandlerFunc(http.MethodGet, "/v1/hotels", app.listHotelsHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/hotels/:hotelID", app.getHotelHandler)
@@ -20,5 +22,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/hotels/:hotelID/reviews", app.listReviewsHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/hotels/:hotelID/reviews/:reviewID", app.getReviewHandler)
 
-	return app.recoverPanic(app.rateLimit(router))
+	return app.metrics(app.recoverPanic(app.rateLimit(router)))
 }
